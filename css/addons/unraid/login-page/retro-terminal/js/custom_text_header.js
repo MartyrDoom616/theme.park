9e3cc22ae0301ff3690c3856a968ed1c48ab4379
MartyrDoom616 <tidmoretyler@gmail.com>
2025-06-09 19:28:24 -0500

    Update custom_text_header.js

--- css/addons/unraid/login-page/retro-terminal/js/custom_text_header.js
+++ css/addons/unraid/login-page/retro-terminal/js/custom_text_header.js
@@ -6,24 +6,11 @@
 // YOU MUST ESCAPE ANY SINGLE BACKSLASHES LIKE SO: \\
 
 let custom_text_header = `
-<div class="custom-text-header"> <pre>                  
-ooooooooooooo  o8o        .o8                                                
-8'   888   `8  `"'       "888                                                
-     888      oooo   .oooo888  ooo. .oo.  .oo.    .ooooo.  oooo d8b  .ooooo. 
-     888      `888  d88' `888  `888P"Y88bP"Y88b  d88' `88b `888""8P d88' `88b
-     888       888  888   888   888   888   888  888   888  888     888ooo888
-     888       888  888   888   888   888   888  888   888  888     888    .o
-    o888o     o888o `Y8bod88P" o888o o888o o888o `Y8bod8P' d888b    `Y8bod8P'
-                                                                             
-                                                                             
-                                                                             
-ooo        ooooo                 .o8   o8o                                   
-`88.       .888'                "888   `"'                                   
- 888b     d'888   .ooooo.   .oooo888  oooo   .oooo.                          
- 8 Y88. .P  888  d88' `88b d88' `888  `888  `P  )88b                         
- 8  `888'   888  888ooo888 888   888   888   .oP"888                         
- 8    Y     888  888    .o 888   888   888  d8(  888                         
-o8o        o888o `Y8bod8P' `Y8bod88P" o888o `Y888""8o                          
-                                                                 </pre> </div>
+<div class="custom-text-header"> <pre>_______     __                             __  ___         ___      
+ /_  __(_)___/ /___ ___  ____  ________     /  |/  /__  ____/ (_)___ _
+  / / / / __  / __ \`__ \\/ __ \\/ ___/ _ \\   / /|_/ / _ \\/ __  / / __ \`/
+ / / / / /_/ / / / / / / / /_/ / /  /  __/  / /  / /  __/ /_/ / / /_/ / 
+/_/ /_/\\__,_/_/ /_/ /_/\\____/_/   \\___/  /_/  /_/\\___/\\__,_/_/\\__,_/
+</pre> </div>
 `;
 document.getElementById("login").innerHTML += custom_text_header
